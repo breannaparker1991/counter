f38ac0ac2c76b8370e5ee50ec7811e8c4666208d
bree parker <breannaparker1991@gmail.com>
2022-04-02 20:21:05 -0600

    code finished

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
-from flask import Flask, session
+from flask import Flask, redirect, render_template, session
 
 app = Flask(__name__)
-app.secret_key = 'keep it secret, keep it safe'
+app.secret_key = 'I love my dog!!!'
+
+@app.route('/')
+def main():
+  if 'count' not in session:
+    session["count"] = 0
+  else:
+    session['count'] += 1
+  return render_template("index.html")
+
+@app.route('/destroy')
+def destroy():
+  session.clear()
+  # session.pop('counter') other way
+  return redirect('/')
+
+@app.route('/increase')
+def increase():
+  session['count'] += 2
+  return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
